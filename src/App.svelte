9c3f78700bd7306e11f728dbb4332b66868c36f8
Hyunbin <68525516+alllearners@users.noreply.github.com>
2022-03-10 00:28:03 +0900

    feat: implement core feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,10 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { handleDrop } from "./lib/functions";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div on:drop|preventDefault={handleDrop} on:dragover|preventDefault>
+    <h1>파일을 여기에 끌어다 놓으세요.</h1>
+    <p>자소가 분리된 파일명을 수정해 다운로드 형태로 제공합니다.</p>
+  </div>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
---
+++ src/lib/functions.ts
@@ -0,0 +1,27 @@
+export const downloadFile = (file: File) => {
+  const anchor = document.createElement('a');
+  anchor.href = URL.createObjectURL(new Blob([file]));
+  anchor.download = file.name.normalize('NFC');
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+};
+
+export const handleDrop = ({ dataTransfer: { items, files } }: DragEvent) => {
+  if (items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (let i = 0; i < items.length; i += 1) {
+      // If dropped items aren't files, reject them
+      if (items[i].kind === 'file') {
+        downloadFile(items[i].getAsFile());
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (let i = 0; i < files.length; i += 1) {
+      downloadFile(files[i]);
+    }
+  }
+};
+
+export default {};
