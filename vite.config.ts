eb08d50dc1b5deb3d6898e80ded985ed907e91b9
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-03-25 12:42:08 +0900

    fix: use absolute url in html head

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,57 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+import type { Plugin, ConfigEnv, UserConfigExport } from 'vite';
+
+// Naver requires absolute url for favicon href
+// Reference https://searchadvisor.naver.com/guide/markup-favicon
+
+const addImages = (domain: string = ''): Plugin => ({
+  name: 'add-favicon-and-og:image-plugin',
+  transformIndexHtml() {
+    return [
+      {
+        injectTo: 'head',
+        tag: 'link',
+        attrs: {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: `${domain}/favicon.svg`,
+        },
+      },
+      {
+        injectTo: 'head',
+        tag: 'link',
+        attrs: {
+          rel: 'icon',
+          type: 'image/png',
+          href: `${domain}/favicon.png`,
+        },
+      },
+      {
+        injectTo: 'head',
+        tag: 'meta',
+        attrs: {
+          property: 'og:image',
+          content: `${domain}/open-graph.jpg`,
+        },
+      },
+    ];
+  },
 });
+
+// https://vitejs.dev/config/
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  const isProduction = mode === 'production';
+
+  const { VITE_PAGE_DOMAIN } = loadEnv(mode, process.cwd());
+
+  const domain = isProduction ? VITE_PAGE_DOMAIN : undefined;
+
+  return defineConfig({
+    plugins: [
+      svelte(),
+      addImages(domain),
+    ],
+  });
+};
